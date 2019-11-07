8174f26de6fc30b3f505d253b9c34c64ef173fb2
Michelle Teresa E de Athayde <michelle.estevinho@hotmail.com>
2019-11-07 14:37:33 -0300

    Ajuste em tela home

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -17,7 +17,7 @@ import { HomeComponent } from './home/home.component';
         SiteRoutingModule,
         FormsModule,
         SharedModule,
-        TimepickerModule
+        TimepickerModule,
     ],
     entryComponents: [
         
