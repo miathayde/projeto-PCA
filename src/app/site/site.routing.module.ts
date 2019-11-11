3cc3de63380dbfb63899d18c97df596cd9a2ece2
Michelle Teresa E de Athayde <michelle.estevinho@hotmail.com>
2019-11-11 15:04:40 -0300

    Criação de rota para sobre, e colocado no menu

--- src/app/site/site.routing.module.ts
+++ src/app/site/site.routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { EadComponent } from './ead/ead.component';
 import { PresenciaisComponent } from './presenciais/presenciais.component';
 import { AtualizacaoComponent } from './atualizacao/atualizacao.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
     { path: 'cursos-pos-presenciais', component: PresenciaisComponent },
     { path: 'cursos-pos-EAD', component: EadComponent },
-    { path: 'cursos-atualizacao', component: AtualizacaoComponent }
+    { path: 'cursos-atualizacao', component: AtualizacaoComponent },
+    { path: 'sobre-pos-unigranrio', component: SobreComponent }
 ];
 
 @NgModule({
