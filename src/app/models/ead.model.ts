33bca838a3d837f9bb6b199d34b8a296a855febf
unknown <michelle.estevinho@hotmail.com>
2019-11-12 00:08:35 -0300

    Criação de ts e model p/ ead e presencial

---
+++ src/app/models/ead.model.ts
@@ -0,0 +1,15 @@
+export class cursosEadModel {
+    nomeCurso: string;
+    duracao: string;
+    modalidade: string;
+    diasAula: string;
+    horario: string;
+    frequencia: string;
+    cargaHoraria: string;
+    titulo: string;
+    imagem: string;
+    publicoAlvo: string;
+    competencias: string;
+    diferencial: string;
+    preRequisito: string;
+}
---
+++ src/app/models/presencial.model.ts
@@ -0,0 +1,16 @@
+export class cursosPresencialModel {
+    nomeCurso: string;
+    duracao: string;
+    modalidade: string;
+    frequencia: string;
+    cargaHoraria: string;
+    titulo: string;
+    imagem: string;
+    publicoAlvo: string;
+    competencias: string;
+    diferencial: string;
+    preRequisito: string;
+    metodologia: string;
+    avaliacao: string;
+    organizacao: string;
+}
