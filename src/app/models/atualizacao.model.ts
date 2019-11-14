b6a4e6bfca406e950ee80676357b271f9eae1820
Michelle Teresa E de Athayde <michelle.estevinho@hotmail.com>
2019-11-14 14:33:21 -0300

    Inclusão de abas nos modais de informação

--- src/app/models/atualizacao.model.ts
+++ src/app/models/atualizacao.model.ts
@@ -8,4 +8,19 @@ export class AtualizacaoModels {
     beneficios: String;
     area: String;
     imagem: String;
+    ementa1: string;
+    ementa2: string;
+    ementa3: string;
+    ementa4: string;
+    ementa5: string;
+    ementa6: string;
+    ementa7: string;
+    ementa8: string;
+    ementa9: string;
+    ementa10: string;
+    ementa11: string;
+    ementa12: string;
+    ementa13: string;
+    ementa14: string;
+    ementa15: string;
 }
